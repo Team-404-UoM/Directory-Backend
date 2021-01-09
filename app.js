54119e6f0a6a805853dc36cc379dbf7f027cbb2c
Anushka-x <anushka.18@itfac.mrt.ac.lk>
2021-01-09 23:31:59 +0530

    Test commit 2

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const signUp = require('./routes/SignUp');
 const event = require('./routes/events');
 const HttpError = require('./models/httperror');
 const cors = require('cors');
-//Test comment
+
 const app = express();
 
 app.use(bodyparser.json());
